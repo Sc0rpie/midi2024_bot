aa2634d504350729b1a755fe90fbdcb74bafed62
Olen <olen2003@gmail.com>
2024-04-22 01:23:28 +0300

    New hint showing up when the previous hint is completed. New group unlock at  halfway point

--- src/commands/complete-hint.ts
+++ src/commands/complete-hint.ts
@@ -21,50 +21,74 @@ export const data = new SlashCommandBuilder()
     .addStringOption(option => option.setName('hint-id').setDescription('ID of a completed hint').setRequired(true))
 
 export async function execute(interaction: CommandInteraction) {
-    // console.log(interaction.options.data[0].value)
-    // console.log(interaction.options.data[1].value)
+    const userId = interaction.options.data[0].value as string;
+    const id = interaction.options.data[1].value as string;
+    const group = id.split('_')[0];
 
-    const userId = interaction.options.data[0].value as string
-    const id = interaction.options.data[1].value as string
-    const group = id.split('_')[0]
+    try {
+        const locationsData = await fs.readFile("./src/data/locations.json");
+        const locationsJson = JSON.parse(locationsData.toString());
 
-    fs.readFile("./src/data/locations.json")
-        .then((data) => {
-            const locationsJson = JSON.parse(data.toString())
-            const hintExists = locationsJson[group]?.locations.some((location: Location) => location.id === id) ?? false
+        if (!(group in locationsJson) || !locationsJson[group].locations.some((location: Location) => location.id === id)) {
+            return interaction.reply({ content: "Hint with such ID does not exist", ephemeral: true });
+        }
 
-            if (!hintExists)
-                return interaction.reply({ content: "Hint with such ID does not exist", ephemeral: true })
+        const teamsData = await fs.readFile("./src/data/teams.json");
+        const teamsJson: Teams = JSON.parse(teamsData.toString());
+        const userTeam = teamsJson.teams.find((team: Team) => team.owner_id === userId);
 
-            fs.readFile("./src/data/teams.json")
-                .then((teamsData) => {
-                    const teamsJson = JSON.parse(teamsData.toString())
-                    const userHasATeam = teamsJson.teams.some((team: Team) => team.owner_id === userId) ?? false
+        if (!userTeam) {
+            return interaction.reply({ content: "User does not have a team", ephemeral: true });
+        }
 
-                    if (!userHasATeam)
-                        return interaction.reply({ content: "User does not have a team", ephemeral: true })
+        if (!userTeam.current_hints.includes(id)) {
+            return interaction.reply({ content: "User does not have this hint unlocked", ephemeral: true });
+        }
 
-                    const userTeam: Team = teamsJson.teams.filter((team: Team) => team.owner_id === userId)[0]
+        // Update current hints and locations found
+        userTeam.current_hints = userTeam.current_hints.filter(hint => hint !== id);
+        userTeam.locations_found.push(id);
 
-                    const userHasHintUnlocked = userTeam.current_hints.some((hintId) => hintId === id)
+        // Determine if the group is exactly halfway completed
+        const groupCompletedCount = userTeam.locations_found.filter(loc => loc.startsWith(group)).length;
+        const groupTotalHints = locationsJson[group].locations.length;
+        const exactlyHalfway = groupCompletedCount === Math.ceil(groupTotalHints / 2);
 
-                    if (!userHasHintUnlocked)
-                        return interaction.reply({ content: "User does not have this hint unlocked", ephemeral: true })
+        const availableHintsInGroup = locationsJson[group].locations.filter((loc: Location) => !userTeam.locations_found.includes(loc.id));
+        let nextHint = availableHintsInGroup.length > 0 ? availableHintsInGroup[Math.floor(Math.random() * availableHintsInGroup.length)] : null;
+        if (nextHint) {
+            userTeam.current_hints.push(nextHint.id);
+        }
 
-                    const teamIndex: number = (teamsJson as Teams).teams.indexOf(userTeam);
+        let extraHint = null;
+        if (exactlyHalfway) {
+            const otherGroups = Object.keys(locationsJson).filter(g => g !== group && !userTeam.unlocked_groups.includes(g));
+            if (otherGroups.length > 0) {
+                const nextGroup = otherGroups[0]; // Assuming a specific selection logic for new group
+                const availableHintsInNextGroup = locationsJson[nextGroup].locations.filter((loc: Location) => !userTeam.locations_found.includes(loc.id));
+                if (availableHintsInNextGroup.length > 0) {
+                    extraHint = availableHintsInNextGroup[Math.floor(Math.random() * availableHintsInNextGroup.length)];
+                    userTeam.current_hints.push(extraHint.id);
+                    if (!userTeam.unlocked_groups.includes(nextGroup)) {
+                        userTeam.unlocked_groups.push(nextGroup);
+                    }
+                }
+            }
+        }
 
-                    (teamsJson as Teams).teams[teamIndex].current_hints = (teamsJson as Teams).teams[teamIndex].current_hints.filter((hintId) => hintId !== id);
-                    (teamsJson as Teams).teams[teamIndex].locations_found.push(id)
+        // Save the updated data
+        await fs.writeFile("./src/data/teams.json", JSON.stringify(teamsJson));
 
-                    fs.writeFile("./src/data/teams.json", JSON.stringify(teamsJson))
-                        .then(() => {
-                            return interaction.reply({ content: "Hint marked as completed", ephemeral: true })
-                        })
-                        .catch((ex) => {
-                            console.log(ex)
-                            return interaction.reply({ content: "An error has occured while marking a hint as completed", ephemeral: true })
-                        })
+        // Construct reply
+        let replyContent = `Hint ${id} marked as completed. Next hint: ${nextHint ? nextHint.hint : 'No more hints available in this group.'}`;
+        if (extraHint) {
+            replyContent += ` Additional hint from new group: ${extraHint.hint}`;
+        }
 
-                })
-        })
+        return interaction.reply({ content: replyContent, ephemeral: true });
+
+    } catch (ex) {
+        console.error(ex);
+        return interaction.reply({ content: "An error has occurred while processing your request", ephemeral: true });
+    }
 }
